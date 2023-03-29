20ace7debfe4ecb6d066df3fb7b193287d191973
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-03-29 13:47:24 -0300

    create style and component Button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Calculator from './main/calculator';
+import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
