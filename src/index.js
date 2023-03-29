e8f34ecec5923079caefdb441e76e4649c68e616
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-03-28 23:29:58 -0300

    Apresentando o compont calculetor

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Calculator from './main/calculator';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>Caculadora</h1>
+      <Calculator />
+    </div>
+    
   </React.StrictMode>
 );
 
