10304c3bfeea5fe7f1681faf6dfcf7cf4c4a14e7
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-03-30 17:08:37 -0300

    Improving and adding more styling to the button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import './Button.css';
 
-export default props =>
-    <button className="button" >{props.label}</button>
+export default props => {
+    let classes = 'button ';
+    classes += props.operation ? 'operation' : '';                              // se a a propriedade operation for passada detro da tag, add operation no className
+    classes += props.double ? 'double' : '';
+    classes += props.triple ? 'triple' : '';
+   
+    return (
+        <button 
+            className={classes}
+            onClick = {e => props.click && props.click(props.label)}>           {/*O && serve para so chamar a função se o elemento é verdadeiro (existe)*/}            
+            {props.label}
+        </button>
+    )
+}
