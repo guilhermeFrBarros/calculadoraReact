6c8f82172f0352db852080ed2b4feda8d0344846
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-04-06 18:43:41 -0300

    Creating initial state and rules for addDigit function

--- src/main/calculator.jsx
+++ src/main/calculator.jsx
@@ -2,36 +2,62 @@ import React, {Component} from "react";
 import "./Calculator.css";
 import Button from "../components/button/Button";
 import Display from "../components/display/Display";
+import { values } from "lodash";
 
-export default class Calculator extends Component {
-    /* OUTRA FORMA DE RESOLVER O PROBLEMA DO THIS
-        constructor(props) {
-            this.clearMemory = this.clearMemory.bind(this) // está fazendo o this da clear memoria apontar para o this da class
-            
-        }
-    */
-    
-    
-    
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0,0],
+    current: 0                  
+}
+
+
+ class Calculator extends Component {
+   
+
+        state = {...initialState}
+
     clearMemory() {
-        console.log('limpar');
+        this.setState( { ...initialState } );
     }
    
     setOperation(operation) {
-        console.log(operation)
+       console.log(operation);
     }
    
-    addDigit(n) {                                   // adiciona numero digitado
-        console.log(n)
+    addDigit(n) {   // adiciona numero digitado ao display
+        if ( n === '.' && this.state.displayValue.includes('.') ) {
+            return;
+        }
+        
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay;                                                     // ou se clearDisplay for true
+        const corruntValue = clearDisplay ? '' : this.state.displayValue;                   // se o display for limpo (clearDisplay) add '' senão add displayValue 
+        const displayValue = corruntValue + n;
+        //console.log(displayValue)
+        this.setState({ displayValue, clearDisplay: false });    // Posso fazer assim também {displayValeu: newDisplayValue}
+    
+        
+        if ( n !== '.' ) {
+            const i = this.state.corruntValue;
+            const newValaue = parseFloat(displayValue);
+            const values = [...this.state.values];
+            values[i] = newValaue;
+            this.setState({values});
+            console.log(values);
+        }
     }
-    render(){                                       // O render() é uma fincionacidade do react para podermos disponibiliazar(apresentar) funcinalidades html
+
+
+    render(){              //so preciso pq é uma class                                                                                 // O render() é uma funcionalidade do react para podermos disponibiliazar(apresentar) funcinalidades html
         const addDigit = n => this.addDigit(n);
         const setOperation = op => this.setOperation(op);
         return (
             <div className="calculator">
-                <Display value={888}/>
+                <Display value={this.state.displayValue}/>
                 <Button label="AC" click = {() => this.clearMemory()} triple />
-                <Button label="/" click = {(e) => setOperation(e)} operation /> {/* click={this.setOperation} */}
+                <Button label="/" click = {(e) => setOperation(e)} operation />                                 {/* click={this.setOperation} */}
                 <Button label="7" click = {(e) => addDigit(e)}/>
                 <Button label="8" click = {(e) => addDigit(e)}/>
                 <Button label="9" click = {(e) => addDigit(e)}/>
@@ -51,3 +77,5 @@ export default class Calculator extends Component {
         )
     }
 }
+
+export default Calculator;
